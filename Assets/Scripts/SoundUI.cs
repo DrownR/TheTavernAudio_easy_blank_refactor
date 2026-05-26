2e58ecbd5cae4cf6955423be4e6eb7fcb2fa5b7f
DrownR <radion2486@gmail.com>
2026-05-26 19:50:00 +0200

    scooby doo

--- Assets/Scripts/SoundUI.cs
+++ Assets/Scripts/SoundUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,19 +7,25 @@ public class SoundUI : MonoBehaviour
     [SerializeField] private VCA vca;
 
     [SerializeField] private Slider globalSlider;
+    [SerializeField] private TMP_Text globalSliderText;
+
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private TMP_Text musicSliderText;
+    
     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private TMP_Text sfxSliderText;
 
 
     private void Start()
     {
-        globalSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/Global",globalSlider.value); });
-        musicSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/Music", musicSlider.value); });
-        sfxSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/SFX", sfxSlider.value); });
+        globalSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/Global",globalSlider.value, globalSliderText); });
+        musicSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/Music", musicSlider.value, musicSliderText); });
+        sfxSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged("vca:/SFX", sfxSlider.value, sfxSliderText); });
     }
 
-    public void OnMusicVolumeChanged(string vcaPath, float value)
+    public void OnMusicVolumeChanged(string vcaPath, float value, TMP_Text text)
     {
         vca.ChangeVolume(vcaPath, value - 100f);
+        text.text = (int)value + "%";
     }
 }
